18a0e1dc475c060d30b427cbde52e3300fae9118
TracyLXC <965016894@qq.com>
2019-07-16 09:47:57 +0800

    Update by LiuXiaocui

--- src/app/service/customer-service.ts
+++ src/app/service/customer-service.ts
@@ -1,10 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { logger } from 'codelyzer/util/logger';
-import { log } from 'util';
-import { catchError } from 'rxjs/operators';
-import { callHooks } from '@angular/core/src/render3/hooks';
 import { UserLogin } from '../model/UserLogin';
 
 const httpOptions = {
